a769b9b7f18e0fc8a57226c14e0b0ef2123ec9de
RishabMangal <rishabhmangal1@gmail.com>
2020-02-18 23:40:41 +0530

    Router removed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import Resume from './Resume'
-import { HashRouter as Router, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route path="/" component={Resume}></Route>
-      </Router>
+      <Resume></Resume>
     </div>
   );
 }
